8a0903952feb317169b8d8cc4a4e02083b780ecf
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-09-15 15:16:03 +0900

    Change behavior of cm command without argument

--- cm.pl
+++ cm.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 if ($#ARGV == -1) {
-    die "Usage: cm <massage>\n";
+    system "git commit";
 } else {
     my $message = join " ", @ARGV;
     system "git commit -m \"$message\"";
