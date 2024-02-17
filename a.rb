762e617ce292e0bb19c5d87954ccb0d8e08ba6cd
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-17 23:55:34 +0900

    Feature: Update a.rb to print alias for ruby files

--- a.rb
+++ a.rb
@@ -3,4 +3,10 @@ Dir.glob "*.pl" do |item|
     puts "alias #{name}=\"perl #{__dir__}/#{item}\""
 end
 
+Dir.glob "*.rb" do |item|
+    name = item[0, item.size - ".rb".size]
+    name == "a" && next
+    puts "alias #{name}=\"ruby #{__dir__}/#{item}\""
+end
+
 system "chmod 755 *.pl"
