051f6a0d99c84484979cbd5877b7d91c935af492
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-18 09:30:23 +0900

    Update cmm.pl

--- cmm.pl
+++ cmm.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 if ($#ARGV == -1) {
-    system "git commit --amend --no-edit";
+    system "git diff --quiet --exit-code --cached && {git commit --amend; true} || git commit --amend --no-edit";
 } else {
     my $message = join " ", @ARGV;
     system "git commit --amend -m \"$message\"";
