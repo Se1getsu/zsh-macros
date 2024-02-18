575a14ff6b72a481750d52e9422609d4bcbb7edf
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-18 13:42:16 +0900

    Refactor cmm.pl

--- cmm.pl
+++ cmm.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 if ($#ARGV == -1) {
-    system "git diff --cached --quiet && {git commit --amend; true} || git commit --amend --no-edit";
+    system "git diff --cached --quiet && { git commit --amend; true; } || git commit --amend --no-edit";
 } else {
     my $message = join " ", @ARGV;
     system "git commit --amend -m \"$message\"";
