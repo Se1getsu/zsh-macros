6b7934bb3a74a234159d913c6e22b66c6d9e464e
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-19 09:08:39 +0900

    Fix: Change cmm to use `if`

--- cmm.pl
+++ cmm.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 if ($#ARGV == -1) {
-    system "git diff --cached --quiet && { git commit --amend; true; } || git commit --amend --no-edit";
+    system "if git diff --cached --quiet; then git commit --amend; else git commit --amend --no-edit; fi";
 } else {
     my $message = join " ", @ARGV;
     system "git commit --amend -m \"$message\"";
